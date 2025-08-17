7517d1c9c680add6e3a228b578096979815b2a74
arefin6 <arefinhossain3@gmail.com>
2025-08-17 12:55:41 +0600

    navbar basic structure complete

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../public/images/logo.png";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full">
+      <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
+        <Image src={logo} alt="logo" />
+        <nav className="space-x-6 hidden md:flex">
+          <Link href="#" className="text-grey-600">
+            About Us
+          </Link>
+          <Link href="#" className="text-grey-600">
+            Contact
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
