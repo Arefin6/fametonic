c7b0903dfa0e0304a916371b4444e64a6367d80d
arefin6 <arefinhossain3@gmail.com>
2025-08-19 21:27:22 +0600

    nav bar all fixes done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -12,32 +12,36 @@ const Navbar = () => {
 
   return (
     <header className="w-full">
-      <div className="max-w-7xl mx-14 flex justify-between items-center p-4">
+      <div className="max-w-7xl mx-auto grid grid-cols-12 items-center p-4">
         {/* Logo */}
-        <Image
-          src={logo}
-          alt="logo"
-          className="w-[104px] md:w-[170px] h-auto"
-        />
+        <div
+          className="col-span-5 flex lg:justify-start
+        justify-center"
+        >
+          <Image
+            src={logo}
+            alt="logo"
+            className="w-[104px] md:w-[170px] h-auto"
+          />
+        </div>
 
         {/* Desktop Nav */}
-        <nav className="hidden md:flex space-x-6 text-[18px] font-semibold text-[#A9A9A9]">
+        <nav className="hidden md:flex col-span-6 justify-end space-x-6 text-[18px] font-semibold text-[#A9A9A9]">
           <Link href="#">About Us</Link>
           <Link href="#">Contact</Link>
         </nav>
 
         {/* Mobile Menu Button */}
-        <button
-          className="md:hidden text-gray-700 mx-[-40px]"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? <X size={28} /> : <Menu size={28} />}
-        </button>
+        <div className="col-span-7 flex justify-end md:hidden">
+          <button className="text-gray-700" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X size={28} /> : <Menu size={28} />}
+          </button>
+        </div>
       </div>
 
       {/* Mobile Dropdown Menu */}
       {isOpen && (
-        <div className="md:hidden flex flex-col  items-end px-4 py-3 space-y-2 text-gray-700 font-semibold">
+        <div className="md:hidden flex flex-col items-end px-4 py-3 space-y-2 text-gray-700 font-semibold">
           <Link href="#" onClick={() => setIsOpen(false)}>
             About Us
           </Link>
